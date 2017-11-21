0094d2d322f94c8ab199fc1f5811e1d373a6fbc9
Jeremy <jpjazzy@hotmail.com>
2017-11-20 16:33:00 -0800

    about page updates

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ This is a log of the hours put into this website
 11/15 - Added sup pages and started inputting information - 2 hours
 11/16 - Added secondary page information and stylings - 3.5 hours
 11/17 - Added autoplay to slider, added about us information, further styled app - 2 hour
+11/19 - Added Tech images to about me section and updated some styling - 1 hr
+11/20 - Styled and input tech about us section and other about page information - 2 hours
+
 ********************************************/
 
 /************* SLIDER JS ************************/
@@ -17,12 +20,12 @@ showSlides(slideIndex);
 
 var transitionSpeed = 8000;
 
-var autoplay = setInterval(function() {plusSlides(1)}, transitionSpeed);
+var autoplay = setInterval(function() {plusSlides(1);}, transitionSpeed);
 
 function userClickPlusSlides(n) {
   clearInterval(autoplay);
   plusSlides(n);
-  autoplay = setInterval(function() {plusSlides(1)}, transitionSpeed);
+  autoplay = setInterval(function() {plusSlides(1);}, transitionSpeed);
 }
 
 function plusSlides(n) {
